654037574665f2ef004f1069a9b61cb98539472a
Aminat Ajagbe <ajagbeaminat95@gmail.com>
2023-09-18 18:00:08 +0100

    Add __ init__ method

--- Backend/Event/models.py
+++ Backend/Event/models.py
@@ -7,9 +7,14 @@ class User(db.Model):
     __tablename__ = "user"
 
     user_id = db.Column(db.Integer, primary_key=True)
-    display_name = db.Column(db.String(), unique=True, nullable=False)
-    email = db.Column(db.String(), unique=True, nullable=False)
-    avatar = db.Column(db.String(), nullable=False)
+    display_name = db.Column(db.String(30), unique=True, nullable=False)
+    email = db.Column(db.String(200), unique=True, nullable=False)
+    avatar = db.Column(db.String(200), nullable=False)
+
+    def __init__(self, display_name, email, avatar):
+        self.display_name = display_name
+        self.email = email
+        self.avatar = avatar
 
     def __repr__(self):
         return f'Display Name: {self.display_name}, Email: {self.email}'
@@ -26,7 +31,7 @@ class User(db.Model):
         db.session.delete(self)
         db.session.commit()
 
-    def user_format(self):
+    def format(self):
         return {
             "user_id": self.user_id,
             "display_name": self.display_name,
@@ -39,36 +44,41 @@ class Event(db.Model):
     __tablename__ = "event"
 
     event_id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(), unique=True, nullable=False)
-    description = db.Column(db.String(), nullable=False)
+    title = db.Column(db.String(50), unique=True, nullable=False)
+    description = db.Column(db.String(200), nullable=False)
     creator = db.Column(db.Integer, db.ForeignKey(
         User.user_id))
-    location = db.Column(db.String(), nullable=False)
+    location = db.Column(db.String(50), nullable=False)
     start_at = db.Column(db.DateTime(), nullable=False)
     end_at = db.Column(db.DateTime(), nullable=False)
-    thumbnail = db.Column(db.String(), nullable=False)
+    thumbnail = db.Column(db.String(200), nullable=False)
+
+    def __init__(self, title, description, creator, location, start_at, end_at, thumbnail):
+        self.title = title
+        self.description = description
+        self.creator = creator
+        self.location = location
+        self.start_at = start_at
+        self.end_at = end_at
+        self.thumbnail = thumbnail
 
     def __repr__(self):
         return f'Title: {self.title}, Description: {self.description}, Creator: {self.creator}, Location: {self.location}, Starts: {self.start_at}, Ends: {self.end_at}'
 
-    def events_to_dict(self):
-        # Dictionary Comprehension to loop through the table
-        return {column.name: getattr(self, column.event_id, column.title, column.description, column.creator, column.location, column.start_at, column.end_at, column.thumbnail) for column in self.__table__.columns}
-
-    def event_insert(self):
+    def insert(self):
         self.creator.verified = True
         db.session.add(self)
         db.session.commit()
 
-    def event_update(self):
+    def update(self):
         self.creator.verified = True
         db.session.commit()
 
-    def event_delete(self):
+    def delete(self):
         db.session.delete(self)
         db.session.commit()
 
-    def event_format(self):
+    def format(self):
         return {
             "event_id": self.event_id,
             "title": self.title,
