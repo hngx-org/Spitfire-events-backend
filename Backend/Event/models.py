0f23fa80bd07591c3989d09161522cf3aa443478
Aminat Ajagbe <ajagbeaminat95@gmail.com>
2023-09-19 09:45:42 +0100

    Add get_uuid function and update user_id default

--- Backend/Event/models.py
+++ Backend/Event/models.py
@@ -1,21 +1,23 @@
 from flask_sqlalchemy import SQLAlchemy
 from Event import db
-import uuid
+from uuid import uuid4
 
 
-# interested_events = db.Table("interested_events",
-#                              db.Column("user_id", db.String(36), db.ForeignKey("user.user_id")),
-#                              db.Column("event_id", db.String(36), db.ForeignKey("event.event_id")))
+def get_uuid():
+    # generates unique id
+    return uuid4().hex
+
 
 class users(db.Model):
     __tablename__ = "users"
-    
-    user_id = db.Column(db.String(36), primary_key=True, default=str(uuid.uuid4()))
+
+    user_id = db.Column(db.String(36), primary_key=True, unique=True,
+                        default=get_uuid, nullable=False)
     display_name = db.Column(db.String(30), unique=True, nullable=False)
     email = db.Column(db.String(200), unique=True, nullable=False)
-    password = db.Column(db.String(200), unique=True, nullable=False)
+    password = db.Column(db.String(16), nullable=False)
     avatar = db.Column(db.String(200), nullable=False)
-    # interests = db.relationship("events", secondary=interested_events, backref="event")
+
     def __init__(self, display_name, password, email, avatar):
         self.display_name = display_name
         self.email = email
@@ -52,7 +54,8 @@ class users(db.Model):
 class events(db.Model):
     __tablename__ = "events"
 
-    event_id = db.Column(db.String(36), primary_key=True, default=str(uuid.uuid4()))
+    event_id = db.Column(db.String(36), primary_key=True,
+                         default=str(uuid.uuid4()))
     title = db.Column(db.String(50), unique=True, nullable=False)
     description = db.Column(db.String(999), nullable=False)
     creator = db.Column(db.String(36), db.ForeignKey(
