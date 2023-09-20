debb4e16376f6677b99997922fd1469a22739597
Dev-wonderful <adeonderful20@gmail.com>
2023-09-20 22:00:32 +0100

    mod

--- Event/user/routes.py
+++ Event/user/routes.py
@@ -1,10 +1,34 @@
-from flask import Blueprint
+"""
+Module containing user-related routes for the Events-App, Team Spitfire.
+"""
+
+from flask import Blueprint, request
+from Event.models import Groups
+
+# from Event.utils import query_paginate_filtered, query_one_filtered
+
 
 users = Blueprint("users", __name__, url_prefix="/api/users")
 
 
 @users.route("/")
 def get_active_signals():
-    """_summary_
+    """
+        Retrieve and return active signals.
+
+    Returns:
+        str: A placeholder return value.
     """
     return
+
+@users.route("/groups", methods=['POST'], strict_slashes=False)
+def create_group():
+    """
+     Create a new group.
+
+    Returns:
+        str: A success message.
+    """
+    title = request.form.get('title')
+    new_group = Groups(title=title)
+    new_group.insert()
