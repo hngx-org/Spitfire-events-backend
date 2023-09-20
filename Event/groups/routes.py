d4bf553a2507bb3c22dbc9f86be941496de4b9fc
Kevin Koech <kevinkoech@proton.me>
2023-09-20 23:43:36 +0300

    add remove_group_member method and fix pylint errorS

--- Event/groups/routes.py
+++ Event/groups/routes.py
@@ -1,9 +1,10 @@
-"""_summary_
+"""
+Module for removing user from a group.
+"""
 
-    Returns:
-        _type_: _description_
-    """
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from Event.models import Users, Groups
+from Event import db
 
 
 groups = Blueprint("groups", __name__, url_prefix="/api/groups")
@@ -11,9 +12,40 @@ groups = Blueprint("groups", __name__, url_prefix="/api/groups")
 
 @groups.route("/")
 def get_active_signals():
-    """_summary_
+    """
+        Retrieve and return active signals.
 
     Returns:
-        _type_: _description_
+        str: A placeholder return value.
     """
     return
+
+@groups.route("/api/groups/<group_id>/members/<user_id>", methods=['DELETE'])
+def remove_group_member(group_id, user_id):
+    """
+    Remove a user from a group.
+
+    Parameters:
+    group_id (str): The ID of the group.
+    user_id (str): The ID of the user to be removed from the group.
+
+    Returns:
+    tuple: A tuple containing response message and status code.
+    """
+    # Retrieve the group and user from the database
+    group_id = Users.query.get(group_id)
+    user_id = Groups.query.get(user_id)
+
+    # Check if the group and user exist
+    if group_id is None or user_id is None:
+        return jsonify({"error": "Group or user not found"}), 404
+
+    # Check if the user is a member of the group
+    if user_id not in group_id.members:
+        return jsonify({"error": "User is not a member of the group"}), 400
+
+    # Remove the user from the group
+    group_id.members.remove(user_id)
+    db.session.commit()
+
+    return jsonify({"message": "User remove from group successfully"}), 200
