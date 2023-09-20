a9a9062240f76f99cc9180b10cd588f652ba1282
Destiny Saturday <84413505+DestinedCodes@users.noreply.github.com>
2023-09-20 15:05:58 +0100

    Delete test_endpoints.py

--- test_endpoints.py
+++
@@ -1,8 +0,0 @@
-"""Just testing this endpoint"""
-import requests
-
-BASE_URI = "http://127.0.0.1:5000"
-
-data = requests.post(f'{BASE_URI}/api/users/groups', data={'title': 'Team spitfire'})
-res = data.text
-print(res)
