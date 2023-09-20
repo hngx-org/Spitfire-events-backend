af347c2a1aa0ae19eef60e8b8f9163ac2ae92b71
Adesina Mubarak <adesinamubarak123@gmail.com>
2023-09-20 15:44:07 +0100

    remove unnecessariry import

--- Event/user/routes.py
+++ Event/user/routes.py
@@ -20,23 +20,9 @@ def create_group():
 
     return 'success'
 
-from Event.models import User, UserGroups
-from flask import jsonify, Blueprint
-
-from Event.utils import query_paginate_filtered, query_one_filtered
-
-
-users = Blueprint("users", __name__, url_prefix="/api/users")
-
-
-@users.route("/")
-def get_active_signals():
-    return
-
-
 @users.route("/api/groups/:groupId/members/:userId", methods=['DELETE'])
 def remove_group_member(group_id, user_id):
-    group_id = User.query.get(group_id)
+    group_id = Users.query.get(group_id)
     user_id = Groups.query.get(user_id)
     
     return jsonify({"message": "User remove from group successfully"}), 200
