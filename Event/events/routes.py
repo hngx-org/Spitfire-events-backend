301d7bc081818023480e1245f6d5ae1f2af7ecf7
Idongesit Inyang <inyangidongesit22@gmail.com>
2023-09-20 15:58:39 +0100

    this is the get group details

--- Event/events/routes.py
+++ Event/events/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from Event.models import Users
+from Event.models import Users, Groups
 from Event.utils import (
     query_one_filtered,
     query_paginate_filtered,
@@ -13,3 +13,33 @@ events = Blueprint("events", __name__, url_prefix="/api/events")#url_prefix incl
 @events.route("/", methods=["POST"])
 def add_provider():
     return
+
+
+groups = Blueprint("groups", __name__, url_prefix="/api/groups")
+@groups.route("/<string:groupId>", methods=["GET"])
+def get_group_by_id(groupId):
+    try:
+        group = Groups.query.filter_by(group_id=groupId).first()
+        
+        if group:
+            group_details = {
+                "group_id": group.group_id,
+                "title": group.title
+            }
+            return jsonify({
+                "status": "success",
+                "message":"Group details successfully fetched",
+                "data": group_details
+            })
+        else:
+            return jsonify({
+                "status": "failed",
+                "message": f"Group with groupId {groupId} not found"
+            }),404
+    except Exception as e:
+        print(f'{type(e).__name__}: {e}')
+        return jsonify({
+            'status': 'failed',
+            'message': 'An error occurred while fetching group details'
+        }), 500
+    
