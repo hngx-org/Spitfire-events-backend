cc26f03c16686068356b0b2b6517bb8b8e257ae0
Adesina Mubarak <adesinamubarak123@gmail.com>
2023-09-20 15:39:58 +0100

    set up delete user group route

--- Event/user/routes.py
+++ Event/user/routes.py
@@ -10,3 +10,33 @@ users = Blueprint("users", __name__, url_prefix="/api/users")
 @users.route("/")
 def get_active_signals():
     return
+
+@users.route("/groups", methods=['POST'], strict_slashes=False)
+def create_group():
+    """just testing"""
+    title = request.form.get('title')
+    new_group = Groups(title=title)
+    new_group.insert()
+
+    return 'success'
+
+from Event.models import User, UserGroups
+from flask import jsonify, Blueprint
+
+from Event.utils import query_paginate_filtered, query_one_filtered
+
+
+users = Blueprint("users", __name__, url_prefix="/api/users")
+
+
+@users.route("/")
+def get_active_signals():
+    return
+
+
+@users.route("/api/groups/:groupId/members/:userId", methods=['DELETE'])
+def remove_group_member(group_id, user_id):
+    group_id = User.query.get(group_id)
+    user_id = Groups.query.get(user_id)
+    
+    return jsonify({"message": "User remove from group successfully"}), 200
