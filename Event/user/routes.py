e8a14c4a4cf513e583711cd4ecefa070d40c30bb
Kevin Koech <kevinkoech@proton.me>
2023-09-20 23:41:29 +0300

    delete remove_group_member method

--- Event/user/routes.py
+++ Event/user/routes.py
@@ -2,9 +2,8 @@
 Module containing user-related routes for the Events-App, Team Spitfire.
 """
 
-from flask import jsonify, Blueprint, request
-from Event.models import Users, Groups
-from Event import db
+from flask import Blueprint, request
+from Event.models import Groups
 
 # from Event.utils import query_paginate_filtered, query_one_filtered
 
@@ -33,35 +32,3 @@ def create_group():
     title = request.form.get('title')
     new_group = Groups(title=title)
     new_group.insert()
-
-    return 'success'
-
-@users.route("/api/groups/<group_id>/members/<user_id>", methods=['DELETE'])
-def remove_group_member(group_id, user_id):
-    """
-    Remove a user from a group.
-
-    Parameters:
-    group_id (str): The ID of the group.
-    user_id (str): The ID of the user to be removed from the group.
-
-    Returns:
-    tuple: A tuple containing response message and status code.
-    """
-    # Retrieve the group and user from the database
-    group_id = Users.query.get(group_id)
-    user_id = Groups.query.get(user_id)
-
-    # Check if the group and user exist
-    if group_id is None or user_id is None:
-        return jsonify({"error": "Group or user not found"}), 404
-
-    # Check if the user is a member of the group
-    if user_id not in group_id.members:
-        return jsonify({"error": "User is not a member of the group"}), 400
-
-    # Remove the user from the group
-    group_id.members.remove(user_id)
-    db.session.commit()
-
-    return jsonify({"message": "User remove from group successfully"}), 200
