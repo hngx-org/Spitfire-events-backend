840bed7ce019b0cd5a0fc3345bb37be3e2eec180
Freeman-kuch <freemanoparaoch@gmail.com>
2023-09-21 18:35:08 +0100

    groups table

--- run.py
+++ run.py
@@ -4,6 +4,26 @@ from Event import create_app
 
 app = create_app()
 
+@app.before_request
+def is_logged_in(session):
+    """
+    Ensures a user is logged in returns error
+
+    Parameterss:
+        session(dict):
+            - flask session object
+
+    returns
+        id(str):
+            - logged in users id
+    """
+    user = session.get("user")
+
+    if not user:
+        raise CustomError("Unauthorized", 401, "You are not logged in")
+
+    return user.get("id")
+
 if __name__ == "__main__":
     app.run(debug=True)
     
