e6672f9039e3ab8c4064f7f60ddacece18b139a6
AJAGBE AMINAT <ajagbeaminat95@gmail.com>
2023-09-21 11:26:55 +0000

    Add delete_event endpoint

--- Event/events/routes.py
+++ Event/events/routes.py
@@ -5,10 +5,28 @@
 from flask import Blueprint, request, jsonify
 from Event.models.images import Images
 from Event.models.comments import Comments
-from Event.utils import query_all_filtered
+from Event.models import Events
+from Event.utils import query_all_filtered, query_one_filtered
 
 # url_prefix includes /api/events before all endpoints in blueprint
 events = Blueprint("events", __name__, url_prefix="/api/events")
+
+# DELETE /api/events/:eventId: Delete an event
+@events.route("/<id>", methods=["DELETE"])
+def delete_event(id):
+    """Delete an event
+    Args:
+        id (str): The id of the event
+    """
+
+    try:
+        del_event = query_one_filtered(table=Events, id=id)
+
+        if del_event:
+            del_event.delete()
+            return jsonify(response={"success": "Event deleted"}), 204
+    except Exception as error:
+        return jsonify(error={"Not Found": "Event not found"}), 404
 
 
 # POST /api/events/<str:event_id>/comments: Add a comment to an event
