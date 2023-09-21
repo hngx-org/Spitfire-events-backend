f6a3c22cad45ee9c4635a60d8a9d654ec6f71362
Freeman-kuch <freemanoparaoch@gmail.com>
2023-09-21 18:36:28 +0100

    groups table

--- run.py
+++ run.py
@@ -4,25 +4,25 @@ from Event import create_app
 
 app = create_app()
 
-@app.before_request
-def is_logged_in(session):
-    """
-    Ensures a user is logged in returns error
+# @app.before_request
+# def is_logged_in(session):
+#     """
+#     Ensures a user is logged in returns error
 
-    Parameterss:
-        session(dict):
-            - flask session object
+#     Parameterss:
+#         session(dict):
+#             - flask session object
 
-    returns
-        id(str):
-            - logged in users id
-    """
-    user = session.get("user")
+#     returns
+#         id(str):
+#             - logged in users id
+#     """
+#     user = session.get("user")
 
-    if not user:
-        raise CustomError("Unauthorized", 401, "You are not logged in")
+#     if not user:
+#         raise CustomError("Unauthorized", 401, "You are not logged in")
 
-    return user.get("id")
+#     return user.get("id")
 
 if __name__ == "__main__":
     app.run(debug=True)
