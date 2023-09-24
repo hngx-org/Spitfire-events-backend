41490c6c3b395c5b3f38607a9f568d20770ae6a8
Destiny Saturday <84413505+DestinedCodes@users.noreply.github.com>
2023-09-24 11:43:22 +0100

    Fix typoo

--- Event/groups/routes.py
+++ Event/groups/routes.py
@@ -353,7 +353,7 @@ def get_all_groups():
         ), 200
 
     except Exception as e:
-        print(str(e)0)
+        print(str(e))
         return jsonify(
             {
                 "error": "Bad Request",
